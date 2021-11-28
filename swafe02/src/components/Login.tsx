dc84ad1b399292a78b53716215bfba1cf030aad5
HamannMojn <jacob@frost-hamann.dk>
2021-11-28 23:35:20 +0100

    implemented navigation

--- swafe02/src/components/Login.tsx
+++ swafe02/src/components/Login.tsx
@@ -1,15 +1,16 @@
-import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
 import authService from '../services/auth.service';
 
 
 export default function Login() {
 
-    const {register, handleSubmit, reset} = useForm();
+    const {register, handleSubmit} = useForm();
+    const navigate = useNavigate();
 
     const onLogin = async (data: { email: string; password: string; }) => {
         authService.login(data.email, data.password);
-        reset();
+        navigate('/Dashboard');
     }
   return (
       <div className="react-hooks-form">
