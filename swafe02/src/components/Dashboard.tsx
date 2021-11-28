3e339d38cb0b21fa3f5fc0e14ba39c854bf4c04e
HamannMojn <jacob@frost-hamann.dk>
2021-11-28 21:48:12 +0100

    implemented userInfo

--- swafe02/src/components/Dashboard.tsx
+++ swafe02/src/components/Dashboard.tsx
@@ -4,39 +4,21 @@ import authService from '../services/auth.service';
 import dataService from '../services/data.service';
 import { WorkoutProgram } from '../Models/WorkoutProgram';
 import '../Styles/dashboard.css';
+import WorkoutProgramComponent from './WorkoutProgram';
 
 
 function Dashboard() {
     const [user, setUser] = useState<User>();
-    const [programs, setPrograms] = useState<Array<WorkoutProgram>>([]);
-    useEffect(() => {
-        getData();
-    }, []);
-
-    const getData = () => {
-        setUser(authService.getCurrentUser());
-        dataService.getWorkoutPrograms()
-        .then((response: any) => {
-            setPrograms(response.data);
-            console.log(response.data);
-        });
-    }
+    
 
     return (
         <div className="list row">
             <h3>Hi {user?.firstName}!</h3>
-            <div className="col-md-6">
-                <h4>WorkoutPrograms</h4>
-                <ul>
-                    {programs && programs.map((program) => (
-                        <li className="card">
-                            {program.workoutProgramId} {program.name}
-                            <p>
-                                {program.description}
-                            </p>
-                        </li>
-                    ))}
-                </ul>
+            <div className="gridcontainer">
+                <div className="grid-item">
+                <WorkoutProgramComponent></WorkoutProgramComponent>
+                </div>
+                
             </div>
             
         </div>
